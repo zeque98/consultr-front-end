685383b68279f57a997c0783d6a28f11d27cee9d
Ezequiel Martinez <zeque_martinez@hotmail.com>
2024-01-22 19:57:26 -0300

    Remove PWA plugin and related configurations

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,7 @@ import App from 'App'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { registerSW } from 'virtual:pwa-register'
 import './index.css'
-
-registerSW()
 
 const MAX_RETRIES = 1
 const queryClient = new QueryClient({
